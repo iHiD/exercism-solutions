278f682e6efcb0ef7760d2fe16713b3dccf860d2
root <root@ip-10-1-0-165.eu-west-2.compute.internal>
2025-06-05 04:35:38 +0000

    [Sync Iteration] ruby/pig-latin/6

---
+++ solutions/ruby/pig-latin/6/pig_latin.rb
@@ -0,0 +1,40 @@
+module PigLatin
+
+  def self.translate(phrase)
+    phrase.split.map { |word| Translator.translate(word) }.join(" ")
+  end
+
+  class Translator
+    def self.translate(word)
+      new(word).translate
+    end
+
+    def initialize(word)
+      @word = word
+    end
+
+    def translate
+      case
+      when starts_with_vowel?
+        word + "ay"
+      else
+        first_sound = word[FIRST_CONSONANT_SOUND]
+        word[first_sound.length..-1] + first_sound + "ay"
+      end
+    
+    end
+
+    private
+    attr_reader :word
+
+    def starts_with_vowel?
+      word.start_with?(*VOWEL_SOUNDS)
+    end
+
+    VOWEL_SOUNDS = %w{a e i o u xr yt}
+    CONSONANT_SOUNDS = %w{ch squ qu thr th sch yt rh} + ('a'..'z').to_a - VOWEL_SOUNDS
+    FIRST_CONSONANT_SOUND = /^#{CONSONANT_SOUNDS.join("|")}/
+
+    private_constant :VOWEL_SOUNDS, :CONSONANT_SOUNDS, :FIRST_CONSONANT_SOUND
+  end
+end
