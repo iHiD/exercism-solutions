a5a247d6988dcbca6e85f035827fa5609204fe87
root <root@ip-10-1-0-165.eu-west-2.compute.internal>
2025-06-05 04:35:37 +0000

    [Sync Iteration] ruby/clock/3

---
+++ solutions/ruby/clock/3/clock.rb
@@ -0,0 +1,33 @@
+class Clock
+  MINS_PER_HOUR = 60
+  HOURS_PER_DAY = 24
+  DAILY_MINS = HOURS_PER_DAY * MINS_PER_HOUR
+
+  def initialize(hour: 0, minute: 0)
+    @time = (hour * MINS_PER_HOUR + minute) % DAILY_MINS
+  end
+
+  def to_s
+    "%02d:%02d" % time.divmod(MINS_PER_HOUR)
+  end
+
+  def +(other)
+    Clock.new(minute: time + other.time)
+  end
+
+  def -(other)
+    Clock.new(minute: time - other.time)
+  end
+
+  def ==(other)
+    time == other.time
+  end
+  alias :eql? :==
+
+  protected
+  attr_reader :time
+end
+
+module BookKeeping
+  VERSION = 3
+end
