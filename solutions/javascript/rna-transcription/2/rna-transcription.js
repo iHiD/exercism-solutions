4dc570eab5b72546de200a658bb91c12adecd2b4
root <root@ip-10-1-0-165.eu-west-2.compute.internal>
2025-06-05 04:35:42 +0000

    [Sync Iteration] javascript/rna-transcription/2

---
+++ solutions/javascript/rna-transcription/2/rna-transcription.js
@@ -0,0 +1,6 @@
+export const toRna = (dna) => {
+  return dna.replace(letterRegex, (nuc) => nucleotideComplements[nuc])
+};
+
+const nucleotideComplements = {"G": "C", "C": "G", "T": "A", "A": "U"}
+const letterRegex = /[A-Z]/g
