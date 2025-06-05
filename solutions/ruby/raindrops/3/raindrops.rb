a0bad20e4b03379784352ab83dafac0d26b02404
root <root@ip-10-1-0-165.eu-west-2.compute.internal>
2025-06-05 04:35:36 +0000

    [Sync Iteration] ruby/raindrops/3

---
+++ solutions/ruby/raindrops/3/raindrops.rb
@@ -0,0 +1,15 @@
+module Raindrops
+  SOUNDS = {3 => "Pling", 5 => "Plang", 7 => "Plong"}.freeze
+
+  def self.convert(num)
+    rhythm = SOUNDS.select{ |key, sound| num % key == 0}.values
+    rhythm.empty?? num.to_s : rhythm.join("")
+  end
+end
+
+
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
+
+
