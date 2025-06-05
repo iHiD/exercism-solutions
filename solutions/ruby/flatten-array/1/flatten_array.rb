177fa21a43e18a604637e231791cf649b37ed1ee
root <root@ip-10-1-0-165.eu-west-2.compute.internal>
2025-06-05 04:35:37 +0000

    [Sync Iteration] ruby/flatten-array/1

---
+++ solutions/ruby/flatten-array/1/flatten_array.rb
@@ -0,0 +1,10 @@
+module FlattenArray
+  def self.flatten(input)
+    # The most ideomatic answer
+    input.flatten.compact
+
+    # Without flatten or compact
+    input.reject(&:nil?).
+          reduce([]) {|output,elem| output += elem.is_a?(Array) ? flatten(elem) : [elem] }
+  end
+end
